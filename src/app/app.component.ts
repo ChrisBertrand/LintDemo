91ef392c4f730f2c0e3cf3eada909affeb9a5669
Chris Bertrand <cjb_bjc@hotmail.com>
2021-01-19 22:06:23 +0000

    Initial Unlinted Code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'LintDemo';
+
+  ngOnInit(): void {
+    const TITLE = this.title;
+
+    var title = 'hey folks';
+
+    var total = 30.2;
+
+    var half = total / 2;
+
+    if (TITLE == 'LintDemo') {
+      console.log('Debugging');
+    }
+  }
 }
